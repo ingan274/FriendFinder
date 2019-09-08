db238068d075998d85ad454f0c2a306e38b55b8a
ingan <isabelngan27@gmail.com>
2019-09-07 19:15:08 -0700

    adding package JSON and servey js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friendsArray = [
     {
         name: "Alex",
-        photoURL: "",
+        photoURL: "https://images.unsplash.com/photo-1506919258185-6078bba55d2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1630&q=80",
         scores: ["3", "2", "1", "5", "4", "4", "3", "1", "2", "2"]
     },
     {
         name: "Eric",
-        photoURL: "",
+        photoURL: "https://images.unsplash.com/photo-1541656300774-69cddcdd9ac1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
         scores: ["4", "4", "4", "3", "5", "2", "3", "5", "3", "5"]
     },
     {
         name: "Kiera",
-        photoURL: "",
+        photoURL: "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80",
         scores: ["5", "2", "2", "1", "3", "1", "3", "2", "5", "5"]
     },
     {
         name: "Brianna",
-        photoURL: "",
+        photoURL: "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
         scores: ["3", "4", "4", "3", "4", "3", "3", "5", "3", "2"]
     }
 ];
